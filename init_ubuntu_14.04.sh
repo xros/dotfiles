4e94e030b0e6aecf5a7cce9cf345d759a876586e
alex <alex@makemakego.com>
2015-05-15 09:37:39 +0800

    add .muttrc for mutt Email client

---
+++ init_ubuntu_14.04.sh
@@ -0,0 +1,6 @@
+# Those packages should be installed at the very beginning
+sudo apt-get install filezilla mysql-workbench postgresql pgadmin3 sqlite3 sqlitebrowser thunderbird virtualbox wireshark arduino stardict gparted qgis fritzing gbrainy uget transmission amule vidalia gargoyle-free blender gimp dia ardour audacity inkscape pitivi cheese mixxx vlc cutecom fbreader midori ekiga -y
+
+
+# Later then,
+# Many more such as: tor, hipchat, quassel, mqtt-spy, jmeter, netbeans, pycharm, astrill and etc. will be installed by hand.
